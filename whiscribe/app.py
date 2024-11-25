13fd172d787fd926388599c222c22c3eda04bb67
silentsoft <hyesung.lee@silentsoft.org>
2024-11-25 23:22:01 +0900

    Apply caching for model selection and file copying

--- whiscribe/app.py
+++ whiscribe/app.py
@@ -7,6 +7,21 @@ from audio import get_audio_tracks, extract_audio_track
 from srt import convert_segments_to_srt
 
 
+@st.cache_data
+def copy_file_to_temp_dir(file):
+    _copied_file = None
+    if file is not None:
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(file.read())
+            _copied_file = temp_file.name
+    return _copied_file
+
+
+@st.cache_resource
+def load_transcriber(_model_size):
+    return Transcriber(_model_size)
+
+
 st.title("Whiscribe")
 st.subheader("Audio to Subtitles")
 
@@ -18,38 +33,31 @@ default_language_index = list(tokenizer.LANGUAGES.keys()).index(system_language_
 language_name = st.selectbox("Select Whisper Language", list(tokenizer.LANGUAGES.values()), index=default_language_index)
 language_code = tokenizer.TO_LANGUAGE_CODE[language_name]
 
-audio_file = st.file_uploader("Select an audio file (mp3, wav, mp4)", type=["mp3", "wav", "mp4"])
+selected_audio_file = st.file_uploader("Select an audio file (mp3, wav, mp4)", type=["mp3", "wav", "mp4"])
+if selected_audio_file is not None:
+    temp_file_path = copy_file_to_temp_dir(selected_audio_file)
+    audio_tracks = get_audio_tracks(temp_file_path)
+    if audio_tracks:
+        selected_index = 0
+        if len(audio_tracks) > 1:
+            track_options = [
+                f"Track {i + 1}: {track['codec_name']} ({track['tags'].get('language', 'unknown')})"
+                for i, track in enumerate(audio_tracks)]
+            selected_track = st.radio("Select an audio track to extract", track_options)
+            selected_index = track_options.index(selected_track)
 
-if audio_file is not None:
-    transcriber = Transcriber(model_size)
-
-    with tempfile.NamedTemporaryFile() as temp_file:
-        temp_file.write(audio_file.read())
-        temp_file_path = temp_file.name
-
-        if audio_file.type == "video/mp4":
-            audio_tracks = get_audio_tracks(temp_file_path)
-
-            if audio_tracks:
-                track_options = [f"Track {i + 1}: {track['codec_name']} ({track['tags'].get('language', 'unknown')})"
-                                 for i, track in enumerate(audio_tracks)]
-                selected_track = st.radio("Select an audio track to extract", track_options)
-                selected_index = track_options.index(selected_track)
-
-                st.write("Converting selected audio track to MP3...")
+        run = st.button("Generate", use_container_width=True)
+        if run:
+            with st.spinner("Extracting audio tracks"):
                 audio_file_path = extract_audio_track(temp_file_path, selected_index, output_format="mp3")
-                st.success(f"Audio track {selected_track} converted to MP3.")
-            else:
-                st.error("No audio tracks found in the uploaded MP4 file.")
-                audio_file_path = None
-        else:
-            audio_file_path = temp_file_path
 
-        if audio_file_path is not None:
-            run = st.button("Generate", use_container_width=True)
-            if run:
+            transcriber = load_transcriber(model_size)
+            with st.spinner("Generating subtitles"):
                 segments = transcriber.transcribe(audio_file_path, language_code)
                 srt_content = convert_segments_to_srt(segments)
 
-                st.success("Subtitles generated successfully!")
-                st.download_button(f"Download {audio_file.name}.srt", data=srt_content, file_name=f"{audio_file.name}.srt")
+            st.success("Subtitles generated successfully!")
+            st.download_button(f"Download {selected_audio_file.name}.srt", data=srt_content,
+                               file_name=f"{selected_audio_file.name}.srt")
+    else:
+        st.error("No audio tracks found in the file.")
