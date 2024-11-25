694210a15d40a6f0e79bac065074998af86140de
silentsoft <hyesung.lee@silentsoft.org>
2024-11-26 02:13:19 +0900

    Fix issue with repetitive sentence generation

--- whiscribe/transcriber.py
+++ whiscribe/transcriber.py
@@ -10,5 +10,8 @@ class Transcriber:
         self.model = whisper_timestamped.load_model(model_size)
 
     def transcribe(self, audio_file, language="en"):
-        result = whisper_timestamped.transcribe(self.model, audio_file, language=language)
+        result = whisper_timestamped.transcribe(self.model,
+                                                audio_file,
+                                                language=language,
+                                                condition_on_previous_text=False)
         return result["segments"]
